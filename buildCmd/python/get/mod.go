2d0de9423e075db77b52a0392ce4bbf5d5efa567
inadati <dr.inadati@gmail.com>
2022-09-10 13:14:57 +0900

    1階層持て余す為3階層構造に戻す

--- buildCmd/python/get/mod.go
+++ buildCmd/python/get/mod.go
@@ -1,49 +1,36 @@
 package get
 
-var clerkRootTemplate = `{{ $location := .Clerk.Location -}}
-{{ range .Clerk.Modules -}}
-import {{ $location }}.{{ .Name }}
+var mod0Template = `{{ $location := .Mod0.Location -}}
+{{ range .Mod0.Modules -}}
+import clerk.{{ $location }}.{{ .Name }}
 {{ end -}}
 {{ printf "\n" -}}
 {{ printf "\n" -}}
 def Clerk(location):
     match location:
-{{ range .Clerk.Modules -}}
-{{"        "}}case {{ printf "%q" .Name }}: return {{ $location }}.{{ .Name }}.Clerk{{- printf "\n" -}}
-{{- end -}}
-`
-
-var mod1Template = `{{ $location := .Location -}}
-{{ $mod1_name := .Mod1.Name -}}
-{{ range .Mod1.Upstreams -}}
-import {{ $location }}.{{ $mod1_name }}.{{ .Name }}
-{{ end -}}
-{{ printf "\n" -}}
-{{ printf "\n" -}}
-def Clerk(location):
-    match location:
-{{ range .Mod1.Upstreams -}}
-{{"        "}}case {{ printf "%q" .Name }}: return {{ $location }}.{{ $mod1_name }}.{{ .Name }}.Clerk{{- printf "\n" -}}
+{{ range .Mod0.Modules -}}
+{{"        "}}case {{ printf "%q" .Name }}: return clerk.{{ $location }}.{{ .Name }}.Clerk{{- printf "\n" -}}
 {{- end -}}`
 
-var mod2Template = `{{ $location := .Location -}}
+
+var mod1Template = `{{ $location := .Mod0.Location -}}
 {{ $mod1_name := .Mod1.Name -}}
-{{ $mod2_name := .Mod2.Name -}}
-{{ range .Mod2.Upstreams -}}
-import {{ $location }}.{{ $mod1_name }}.{{ $mod2_name }}.{{ .Name }}
+{{ range .Mod1.Upstreams -}}
+import clerk.{{ $location }}.{{ $mod1_name }}.{{ .Name }}
 {{ end -}}
 {{ printf "\n" -}}
 {{ printf "\n" -}}
 def Clerk(location):
     match location:
-{{ range .Mod2.Upstreams -}}
-{{"        "}}case {{ printf "%q" .Name }}: return {{ $location }}.{{ $mod1_name }}.{{ $mod2_name }}.{{ .Name }}.Clerk{{- printf "\n" -}}
+{{ range .Mod1.Upstreams -}}
+{{"        "}}case {{ printf "%q" .Name }}: return clerk.{{ $location }}.{{ $mod1_name }}.{{ .Name }}.Clerk{{- printf "\n" -}}
 {{- end -}}`
 
-var mod3Template = `{{ $location := .Location -}}
-""" <location: {{ $location }}.{{ .Mod1.Name }}.{{ .Mod2.Name }}.{{ .Mod3.Name }} />
 
-{{.Mod3.Comment}}
+var mod2Template = `{{ $location := .Mod0.Location -}}
+""" <location: {{ $location }}.{{ .Mod1.Name }}.{{ .Mod2.Name }} />
+
+{{.Mod2.Comment}}
 
 """
 
@@ -53,10 +40,9 @@ def Clerk():
 
 func ModuleTemplate(modLevel string) string {
     tmpl := map[string]string{
-        "clerkRoot": clerkRootTemplate,
+        "mod0": mod0Template,
         "mod1": mod1Template,
         "mod2": mod2Template,
-        "mod3": mod3Template,
     }
     return tmpl[modLevel]
 }
