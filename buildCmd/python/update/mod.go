e597eefbd00b5fd9d1eb887eef9ef3944b275763
inadati <dr.inadati@gmail.com>
2022-09-26 21:32:43 +0900

    getやsetなどは一番最後の階層になるよう修正

--- buildCmd/python/update/mod.go
+++ buildCmd/python/update/mod.go
@@ -23,3 +23,41 @@ func EndMod(filePath string, newComment string) error {
 
     return nil
 }
+
+
+func EndModComment(filePath string, newComment string) error {
+    replaceTarget := regexp.MustCompile(`""" <location:[\s\S\n]*?"""`)
+    bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+    fileContents := string(bytes)
+    fileContents = replaceTarget.ReplaceAllString(fileContents, newComment)
+
+    err = ioutil.WriteFile(filePath, []byte(fileContents), os.ModePerm)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+
+func EndModClerk(filePath string, newValue string) error {
+    replaceTarget := regexp.MustCompile(`def clerk[\s\S\n]*# end clerk`)
+    bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+    fileContents := string(bytes)
+    fileContents = replaceTarget.ReplaceAllString(fileContents, newValue)
+
+    err = ioutil.WriteFile(filePath, []byte(fileContents), os.ModePerm)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
