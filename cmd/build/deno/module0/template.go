162e6e71b80287013aab8e02f85ed79418afbdcb
inadati <dr.inadati@gmail.com>
2022-11-16 12:53:35 +0900

    [wip]: denoを追加

---
+++ cmd/build/deno/module0/template.go
@@ -0,0 +1,21 @@
+package module0
+
+type TemplateSpec struct {}
+
+var Template = &TemplateSpec{}
+
+func (s *TemplateSpec) Get() string {
+    return `{{ range .Mod0.Upstream -}}
+import _{{.Name}} from "./{{ .Name }}""
+{{ end -}}
+{{ printf "\n" -}}
+{{ printf "\n" -}}
+export default { clerk }
+function clerk(location) {
+    switch (location) {
+{{ range .Mod0.Upstream -}}
+{{"        "}}case {{ printf "%q" .Name }}: return _{{.Name}}.clerk{{- printf "\n"}}
+{{- end -}}
+{{"    "}}}
+}`
+}
