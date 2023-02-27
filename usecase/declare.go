3114a20e6624d2a570bf942d87466419626921a1
inadati <dr.inadati@gmail.com>
2023-02-27 20:20:49 +0900

    ディレクトリの作成とファイルの作成の成功を確認

--- usecase/declare.go
+++ usecase/declare.go
@@ -20,43 +20,47 @@ func NewDeclareUseCase(r repository.DeclareRepository) DeclareUseCase {
 
 func (u *declareUseCase) GenerateArch() error {
     decl, _ := u.DeclareRepository.Load()
+    conf := decl.TacitConfig
 
     for _, s := range decl.Spec {
+        s.SetTacitConfig(conf)
         workdir := s.ChangeDirectory(".")
         if err := s.CreateDirectory(workdir); err != nil { return err }
 
         if len(s.Upstream) != 0 {
-            if err := generateUpstream(workdir, s.Upstream); err != nil { return err }
+            if err := generateUpstream(workdir, s.Upstream, conf); err != nil { return err }
         }
 
         if len(s.CodeFile) != 0 {
-            if err := generateCodeFile(workdir, s.CodeFile); err != nil { return err }
+            if err := generateCodeFile(workdir, s.CodeFile, conf); err != nil { return err }
         }
     }
 
     return nil
 }
 
-func generateUpstream(prevWorkDir string, upstream []*model.DeclareUpstream) error {
+func generateUpstream(prevWorkDir string, upstream []*model.DeclareUpstream, conf *model.TacitConfig) error {
 
-    for _, u := range upstream {
-        workdir := u.ChangeDirectory(prevWorkDir)
-        if err := u.CreateDirectory(workdir); err != nil { return err }
+    for _, ups := range upstream {
+        ups.SetTacitConfig(conf)
+        workdir := ups.ChangeDirectory(prevWorkDir)
+        if err := ups.CreateDirectory(workdir); err != nil { return err }
 
-        if len(u.Upstream) != 0 {
-            if err := generateUpstream(workdir, u.Upstream); err != nil { return err }
+        if len(ups.Upstream) != 0 {
+            if err := generateUpstream(workdir, ups.Upstream, conf); err != nil { return err }
         }
 
-        if len(u.CodeFile) != 0 {
-            if err := generateCodeFile(workdir, u.CodeFile); err != nil { return err }
+        if len(ups.CodeFile) != 0 {
+            if err := generateCodeFile(workdir, ups.CodeFile, conf); err != nil { return err }
         }
     }
     return nil
 }
 
-func generateCodeFile(workdir string, codeFile []*model.DeclareCodeFile) error {
+func generateCodeFile(workdir string, codeFile []*model.DeclareCodeFile, conf *model.TacitConfig) error {
 
     for _, c := range codeFile {
+        c.SetTacitConfig(conf)
         c.CreateCodeFile(workdir)
     }
     return nil
